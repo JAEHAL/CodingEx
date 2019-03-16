b582d5e11a39a5b8de4331106cda6fb9797342a3
JaeHa KIM <wogk1130@naver.com>
2019-03-16 22:33:22 +0900

    체육복 다시 해야함

--- src/codingEx/Gym_suit.java
+++ src/codingEx/Gym_suit.java
@@ -3,7 +3,7 @@
 
 package codingEx;
 
-//import java.util.Arrays;
+import java.util.Arrays;
 
 public class Gym_suit {
 	public static void main(String[] args) {
@@ -11,7 +11,7 @@ public class Gym_suit {
 		int n = 5; //전체 학생 수
 		int answer = 0;
 		int[] lost = {2,4}; // 체육복을 도난당한 학생들의 번호가 담긴 배열
-		int[] reserve = {3}; // 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열
+		int[] reserve = {1,4}; // 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열
 		int lost_std = 0; // 체육복 잃어버린 학생 수
 		
 		int[] std = new int[n]; // 전체 학생을 담는 배열
@@ -20,51 +20,97 @@ public class Gym_suit {
 		for(int i=0; i<std.length; i++) {
 			std[i] = 1;
 		}
-		//System.out.println(Arrays.toString(std));
-		
-		// 도난당한 학생의 체육복 -1하기
-		for(int i=0; i<lost.length; i++) {
-			std[lost[i]-1] -= 1;
-			lost_std += 1;
-		}
-		//System.out.println(Arrays.toString(std));
-		//System.out.println(lost_std);
+		System.out.println(Arrays.toString(std));
 		
 		// 여벌의 체육복을 가져온 학생 찾아서 +1 시키기
 		for(int i=0; i<reserve.length; i++) {
 			std[reserve[i]-1] += 1;
 		}
-		//System.out.println(Arrays.toString(std));
+		System.out.println(Arrays.toString(std));
+				
+		// 도난당한 학생의 체육복 -1하기
+		for(int i=0; i<lost.length; i++) {
+			std[lost[i]-1] -= 1;
+			lost_std += 1;
+		}
+		System.out.println(Arrays.toString(std));
+		System.out.println(lost_std);
 		
 		//체육복 빌려주는 조건
 		for(int i=0; i<std.length; i++) {
-			if(std[i]==0) {
-				if(i==0 && std[i+1]==2) { // 1번째 학생의 체육복이 도난당했고 두 번째 학생이 여벌의 체육복을 가져왔을 때
-					std[i] += 1;
-					std[i+1] -= 1;
+			if(std[i]==2) {
+				if(i==0 && std[i+1]==0) {
+					std[i+1] += 1;
+					std[i] -= 1;
 					lost_std -= 1;
 				}
-				else if((i==(std.length-1)) && std[std.length-2]==2) { // 마지막 학생 체육복이 0이고, 그 전 학생이 2개일 때
-					std[i] += 1;
-					std[i-1] -= 1;
+				else if(i==std.length-1 && std[std.length-2]==0) {
+					std[i] -= 1;
+					std[i-1] += 1;
 					lost_std -= 1;
 				}
-				else if(i!=0 && std[i+1]==2 ) { // i가 0이 아니고 다음 학생이 체육복이 2일 때
-					std[i] += 1;
-					std[i+1] -= 1;
+				else if(i!=0 && i!=std.length-1 && std[i-1]==0) {
+					std[i] -= 1;
+					std[i-1] += 1;
 					lost_std -= 1;
 				}
-				else if(i!=0 && std[i-1]==2) { // i가 0이 아니고 전 학생이 체육복이 2일 때
-					std[i] += 1;
-					std[i-1] -= 1;
+				else if(i!=0 && i!=std.length-1 && std[i+1]==0) {
+					std[i] -= 1;
+					std[i+1] += 1;
 					lost_std -= 1;
 				}
 			}
+			
 		}
+		System.out.println(Arrays.toString(std)+"a");
+		
+		//체육복을 못 빌려줄때
+		for(int i=0; i<std.length; i++) {
+			if(std[i]==1 || std[i]==0) {
+				if(std[i]==0) {
+					
+				}
+			}
+		}
+//
+//			if(std[i]==0) {
+//				if(i==0 && std[i+1]==2) { // 1번째 학생의 체육복이 도난당했고 두 번째 학생이 여벌의 체육복을 가져왔을 때
+//					std[i] += 1;
+//					std[i+1] -= 1;
+//					lost_std -= 1;
+//					System.out.println(Arrays.toString(std)+"a");
+//				}
+//				else if((i==(std.length-1)) && std[std.length-2]==2) { // 마지막 학생 체육복이 0이고, 그 전 학생이 2개일 때
+//					std[i] += 1;
+//					std[i-1] -= 1;
+//					lost_std -= 1;
+//					System.out.println(Arrays.toString(std)+"b");
+//				}
+//				else if(i!=0 && i!=std.length-1 && std[i+1]==2) { // i가 0이 아니고 다음 학생이 체육복이 2일 때
+//					std[i] += 1;
+//					std[i+1] -= 1;
+//					lost_std -= 1;
+//					System.out.println(Arrays.toString(std)+"c");
+//					System.out.println(lost_std);
+//				}
+//				else if(i!=0 && i!=std.length-1 && std[i-1]==2) { // i가 0이 아니고 전 학생이 체육복이 2일 때
+//					std[i] += 1;
+//					std[i-1] -= 1;
+//					lost_std -= 1;
+//					System.out.println(Arrays.toString(std)+"d");
+//				}
+//				else {
+//					lost_std -= 1;
+//					System.out.println(Arrays.toString(std)+"e");
+//					System.out.println(lost_std+" f");
+//				}
+//			}
+//
 		
 		//System.out.println(Arrays.toString(std));
 		//System.out.println(lost_std);
-		
+		System.out.println(n);
+		System.out.println(lost_std);
 		answer = n - lost_std;
 		
 		System.out.println(answer);		
--- src/codingEx/Same_Number_Hate.java
+++ src/codingEx/Same_Number_Hate.java
@@ -2,8 +2,39 @@
 
 package codingEx;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Same_Number_Hate {
 	public static void main(String[] args) {
-		//int[] arr = {1,1,3,3,0,1,1};
+		int[] arr = {4,4,4,3,3};
+		
+		List<Integer> list = new ArrayList<>();
+		
+		for(int i=0; i<arr.length-1; i++) {
+			if(arr[i] != arr[i+1] && arr[i] != arr[i-1]) { // 양 옆에 중복이 하나도 없는 값 구하기
+				System.out.println(arr[i]+"a");
+				list.add(arr[i]);
+			}
+			
+			else if(arr[i] == arr[i+1]) { // 양 옆이 같을 때
+				if(i==0 || i==arr.length-2) { // 첫 번째 값과 마지막 값이 같은 거는 조건 따로
+					System.out.println(arr[i]+"b");
+					list.add(arr[i]);
+				}
+				else {
+					System.out.println(arr[i]+"c");
+					list.add(arr[i]);
+				}
+			}
+		}
+		
+		int[] answer = new int[list.size()];
+		for(int i=0; i<list.size(); i++) {
+			answer[i] = list.get(i);
+		}
+		
+		System.out.println(Arrays.toString(answer));
 	}
 }

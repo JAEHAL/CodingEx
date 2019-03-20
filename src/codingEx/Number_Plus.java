9626c208615627be7405d9f0efbd10f1a8b6b6a5
JaeHa KIM <wogk1130@naver.com>
2019-03-20 23:36:51 +0900

    두 정수의 합 미완성

---
+++ src/codingEx/Number_Plus.java
@@ -0,0 +1,26 @@
+//두 정수 사이의 합 문제
+
+package codingEx;
+
+public class Number_Plus {
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 6;
+		
+		long answer = 0;
+		int mid = 0;
+		
+		while(true) {
+			if(a>b) {
+				mid = (a+b)/2;
+				
+			}
+			else if(a<b) {
+				
+			}
+			break;
+		}
+		
+		System.out.println(answer);
+	}
+}

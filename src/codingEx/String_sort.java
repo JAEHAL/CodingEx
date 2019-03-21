7bc93561717b6d719e1f51e8ec89f0cb772e2b3b
JaeHa KIM <wogk1130@naver.com>
2019-03-22 00:25:15 +0900

    문자열 내 맘대로 정렬 왜 안되는지 이해x

--- src/codingEx/String_sort.java
+++ src/codingEx/String_sort.java
@@ -6,13 +6,30 @@ import java.util.Arrays;
 
 public class String_sort {
 	public static void main(String[] args) {
-		String[] strings = {"sun","bed","car"};
-		int n = 1;
-		String[] answer = {};
+		String[] strings = {"abce","abcd","cda"};
+		int n = 2;
 		
+		String[] answer = new String[strings.length];
+		
+		String[] num = new String[strings.length]; // 인덱스 n번째 문자 값 넣기 위한 배열
+		
+		// 인덱스 n번째 문자
 		for(int i=0; i<strings.length; i++) {
-			System.out.println(strings[i].length());
-//			System.out.println(strings[i]);
+			num[i] = strings[i].substring(n);
+		}
+		System.out.println(Arrays.toString(num));
+		
+		Arrays.sort(num);
+		
+		System.out.println(Arrays.toString(num));
+		
+		for(int i=0; i<num.length; i++) {
+			for(int j=0; j<strings.length; j++) {
+//				System.out.println(num[i] + " " + strings[j].substring(n));
+				if(num[i] == strings[j].substring(n)) {
+					answer[i] = strings[j];
+				}
+			}
 		}
 		
 		System.out.println(Arrays.toString(answer));

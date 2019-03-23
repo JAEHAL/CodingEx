cd6a8e1ce3f49d0ba94320a464ca54e0ceac6814
JaeHa KIM <wogk1130@naver.com>
2019-03-24 03:31:30 +0900

    문자열 내림차순으로 배치하기 완료

--- src/codingEx/Descending_String.java
+++ src/codingEx/Descending_String.java
@@ -6,26 +6,29 @@ import java.util.Arrays;
 
 public class Descending_String {
 	public static void main(String[] args) {
-		String s = "AcdeaC";
+		//String s = "AcdeaC";
+		String s = "Zbcdefg";
 		String answer = "";
 		
-		char[] s1 = new char[s.length()];
-		
-		for(int i=0; i<s1.length; i++) {
-			s1[i] = s.charAt(i);
-		}
+		char[] s1 = s.toCharArray(); // 문자열을 char로 변환하는 방법
+		char[] s2 = new char[s.length()]; // s1에 담은 배열값들을 거꾸로 담기 위한 배열 선언
 		
 		System.out.println(Arrays.toString(s1));
 		Arrays.sort(s1);
 		System.out.println(Arrays.toString(s1) + " 1");
 		
-		for(int i=s1.length-1; i<=0; i--) {
-			for(int j=0; j<s1.length; j++) {
-				System.out.println(s1[j] + " " + s1[i]);
-				s1[j] = s1[i];
-			}
+		for(int i=s1.length-1; i>=0; i--) {
+			//System.out.println(i);
+			s2[(s1.length-1)-i] = s1[i];
 		}
-		System.out.println(Arrays.toString(s1) + " 2");
+		
+		System.out.println(Arrays.toString(s2) + " 2");
+		answer = String.valueOf(s2); // char 배열을 문자열로 변환하는 방법
+		// char 배열을 문자열로 변환하는 다른 방법
+		/*
+		 * answer = new String(s2);
+		*/
+		
 		System.out.println(answer);
 	}
 }
--- src/codingEx/String_sort.java
+++ src/codingEx/String_sort.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 public class String_sort {
 	public static void main(String[] args) {
 		String[] strings = {"abcca","adcca","cda"};
+		//String[] strings = {"abce","abcd","cdx"};
 		int n = 2;
 		
 		String[] answer = new String[strings.length];
@@ -25,6 +26,9 @@ public class String_sort {
 		
 		for(int i=0; i<num.length; i++) {
 			for(int j=0; j<strings.length; j++) {
+				if(num[i] == num[j]) {
+					
+				}
 				if(num[i].equals(strings[j].substring(n))) {
 					answer[i] = strings[j];
 				}

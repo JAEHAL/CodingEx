8f3b8d4c6c02d294f2b9c1e8ef44550b3d7de2c3
JaeHa KIM <wogk1130@naver.com>
2019-03-26 19:13:22 +0900

    문자열 다루기 기본 문제 완료

---
+++ src/codingEx/String_Deal_Basic.java
@@ -0,0 +1,27 @@
+//문자열 다루기 기본 문제
+
+package codingEx;
+
+public class String_Deal_Basic {
+	public static void main(String[] args) {
+		String s = "1234";
+		boolean answer = true;
+		
+		char temp;
+		
+		for(int i=0; i<s.length(); i++) {
+			temp = s.charAt(i);
+			if(s.length()==4 || s.length()==6) {
+				//아스키코드 48~57까지가 숫자
+				if((int)temp < 48 || (int)temp > 57) {
+					answer = false;
+				}
+			}
+			else {
+				answer = false;
+			}
+		}
+		
+		System.out.println(answer);
+	}
+}

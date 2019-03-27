057ce773948ceb1180c05ef2ee2782cf97d8dde2
JaeHa KIM <wogk1130@naver.com>
2019-03-28 01:54:13 +0900

    소수 찾기 문제 효율성 문제뜸 에라토스테네스의 체로 다시 풀기

---
+++ src/codingEx/Find_Decimal.java
@@ -0,0 +1,23 @@
+//소수 찾기 문제
+
+package codingEx;
+
+public class Find_Decimal {
+	public static void main(String[] args) {
+		int n = 10;
+		int answer = 0;
+		
+		//에라토스테네스의 체를 통해 소수 구하기
+		
+		
+		//n값이 커질수록 효율성 떨어짐
+		/*
+		 * int num = 0;
+		 * 
+		 * for(int i=2; i<=n; i++) { for(int j=2; j<=i; j++) { if(i%j == 0) { num++; } }
+		 * if(num==1) { answer++; } num=0; }
+		 */
+		
+		System.out.println(answer);
+	}
+}

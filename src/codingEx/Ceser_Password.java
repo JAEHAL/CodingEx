82cdf6e0632ebe5244d882a418d3f0b92cca80f0
JaeHa KIM <wogk1130@naver.com>
2019-03-31 03:39:11 +0900

    시저 암호 문제 남이 푼거

--- src/codingEx/Ceser_Password.java
+++ src/codingEx/Ceser_Password.java
@@ -20,14 +20,14 @@ public class Ceser_Password {
 				s1[i] = (char)(s1[i] + n);
 				System.out.println(s1[i]);
 				if(s1[i] >= 91) { // 대문자 범위를 벗어나면
-					s1[i] = (char)(('Z'-'A'+n)%26+'A');
+					s1[i] = (char)((s1[i]-'A'+n)%26+'A');
 					System.out.println(1);
 				}
 			}
 			else if(s1[i] >= 97 && s1[i] <123) { // 값이 소문자이면
 				s1[i] = (char)(s1[i] + n);
 				if(s1[i] >= 123) { // 소문자 범위를 벗어나면
-					s1[i] = (char)(('z'-'a'+n)%26+'a');
+					s1[i] = (char)((s1[i]-'a'+n)%26+'a');
 				}
 			}
 		}
---
+++ src/codingEx/Ceser_Password2.java
@@ -0,0 +1,25 @@
+// 시저 암호 문제 남이 푼거
+
+package codingEx;
+
+public class Ceser_Password2 {
+	public static void main(String[] args) {
+		String s = "a B z";
+		int n = 4;
+		String answer = "";
+		
+		//n = n%26;
+		for(int i=0; i<s.length(); i++) {
+			char ch = s.charAt(i);
+			if(Character.isLowerCase(ch)) { // ch가 소문자이면
+				ch = (char)((ch-'a'+n)%26+'a');
+			}
+			else if(Character.isUpperCase(ch) ) { // ch가 대문자이면
+				ch = (char)((ch-'A'+n)%26+'A');
+			}
+			answer += ch;
+		}
+			
+		System.out.println(answer);
+	}
+}

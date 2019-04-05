b68546417571993b496a1a265d8194ab403403f2
JaeHa KIM <wogk1130@naver.com>
2019-04-06 03:21:00 +0900

    이상한 문자 만들기 수정 및 다시 해야함

--- src/codingEx/Strange_Text.java
+++ src/codingEx/Strange_Text.java
@@ -9,18 +9,25 @@ public class Strange_Text {
 		String s = "try hello world";
 		String answer = "";
 		
-		char[] ch = s.toCharArray();
-		System.out.println(Arrays.toString(ch));
+		String[] list = s.split(" ");
+		System.out.println(Arrays.toString(list));
 		
-		for(int i=0; i<ch.length; i++) {
-			if(i%2==0) {
-				ch[i] = Character.toUpperCase(ch[i]);
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0; i<list.length; i++) {
+			for(int j=0; j<list[i].length(); j++) {
+				if(j%2==0) {
+					sb.append(String.valueOf(list[i].charAt(j)).toUpperCase());
+				}
+				else {
+					sb.append(String.valueOf(list[i].charAt(j)).toLowerCase());
+				}
 			}
-			else if(i%2 != 0) {
-				ch[i] = Character.toLowerCase(ch[i]);
+			if(i<list.length-1) {
+				sb.append(" ");
 			}
 		}
-		answer = String.valueOf(ch);
+		answer = sb.toString();
 		
 		System.out.println(answer);
 	}

d72f879930c82b415688c57e745c359c36b243d8
JaeHa KIM <wogk1130@naver.com>
2019-04-06 13:20:23 +0900

    자연수 뒤집어 배열로 문제 완료

---
+++ src/codingEx/Natural_Reverse.java
@@ -0,0 +1,25 @@
+// 자연수 뒤집어 배열로 만들기 문제
+
+package codingEx;
+
+import java.util.Arrays;
+
+public class Natural_Reverse {
+	public static void main(String[] args) {
+		long n = 12345;
+		int[] answer = {};
+		
+		String s1 = String.valueOf(n);
+		String[] s_Array = new String[s1.length()];
+		
+		for(int i=s1.length()-1; i>=0; i--) {
+			s_Array[(s1.length()-1)-i] = String.valueOf(s1.charAt(i));
+		}
+		
+		//String배열을 int배열로 변환하는 작업
+		answer = Arrays.stream(s_Array).mapToInt(Integer::parseInt).toArray();
+		
+		System.out.println(Arrays.toString(answer));
+		
+	}
+}

f1a7a92353e8ceaeb330cbff40d43dbace5205b7
JaeHa KIM <wogk1130@naver.com>
2019-04-23 17:39:28 +0900

    핸드폰 번호 가리기 문제 완료

---
+++ src/codingEx/Phone_Number.java
@@ -0,0 +1,21 @@
+// 핸드폰 번호 가리기
+
+package codingEx;
+
+public class Phone_Number {
+	public static void main(String[] args) {
+		String phone_number = "027778888";
+		String answer = "";
+		
+		char[] ch = phone_number.toCharArray();
+		
+		for(int i=0; i<ch.length; i++) {
+			if(i<ch.length-4) {
+				ch[i] = '*';
+			}
+		}
+		answer = String.valueOf(ch);
+		
+		System.out.println(answer);
+	}
+}

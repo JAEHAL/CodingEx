75a92f0d99092da1b2f9da7759f497d8050408e2
JaeHa KIM <wogk1130@naver.com>
2019-04-24 01:31:29 +0900

    예산 문제 완료

---
+++ src/codingEx/Budget.java
@@ -0,0 +1,23 @@
+// 예산 문제
+
+package codingEx;
+
+import java.util.Arrays;
+
+public class Budget {
+	public static void main(String[] args) {
+		int[] d = {1,3,2,5,4};
+		int budget = 9;
+		int answer = 0;
+		
+		Arrays.sort(d);
+		for(int i=0; i<d.length; i++) {
+			if(budget-d[i] >= 0) {
+				budget -= d[i];
+				answer++;
+			}
+		}
+		
+		System.out.println(answer);
+	}
+}

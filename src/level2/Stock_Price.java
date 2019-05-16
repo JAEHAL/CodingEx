7747865b8b55388809fa3add94e99b9c6720e6fd
JaeHa KIM <wogk1130@naver.com>
2019-05-16 23:43:06 +0900

    주식가격 문제 완료

--- src/level2/Stock_Price.java
+++ src/level2/Stock_Price.java
@@ -2,11 +2,31 @@
 
 package level2;
 
+import java.util.Arrays;
+
 public class Stock_Price {
 	public static void main(String[] args) {
 		int[] prices = {1,2,3,2,3};
-		int[] answer = {};
+		int[] answer = new int[prices.length];
 		
-		System.out.println(answer);
+		answer[prices.length-1] = 0;
+		int num = 0;
+		
+		for(int i=0; i<prices.length-1; i++) {
+			for(int j=i+1; j<prices.length; j++) {
+				if(prices[i]<=prices[j]) {
+					num++;
+				}
+				else {
+					num++;
+					break;
+				}
+			}
+			answer[i] = num;
+			num=0;
+		}
+		
+		System.out.println(Arrays.toString(answer));
 	}
 }
+ 
